65035744f091f5277581462ea7d34d570208bacf
George Ng <just4jcgeorge@gmail.com>
2023-05-01 09:39:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Meanu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-
+streamlit.text('Hard-Boiled Free-Range Egg')
